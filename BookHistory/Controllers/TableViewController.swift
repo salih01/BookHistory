7caa5138c2928f0901a55efb9f0bc5c52006172f
Salih Çakmak <slhckmk01@gmail.com>
2022-11-16 01:46:50 +0300

    Project Finished

--- BookHistory/Controllers/TableViewController.swift
+++ BookHistory/Controllers/TableViewController.swift
@@ -25,11 +25,59 @@ extension TableViewController:UITableViewDelegate,UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+           
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
+            let idString = idArray[indexPath.row].uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@",idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID{
+                            if id == idArray[indexPath.row]{
+                                context.delete(result)
+                                self.bookNameArray.remove(at: indexPath.row)
+                                self.idArray.remove(at: indexPath.row)
+                                tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                }
+                                catch{
+                                    print("save alınmadı")
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch{
+                print("Fine I do it my self ")
+            }
+            
+            
+
+            
+            
+          
+            
+            
+
+        }
+    }
     
 }
 
 
 class TableViewController: UIViewController {
+    
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -44,6 +92,10 @@ class TableViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        
+        bookNameArray.removeAll()
+        idArray.removeAll()
         
         getData()
       
